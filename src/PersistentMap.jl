99b3e8152bc96d77323c46e533fe4e77ad231569
Noah C. Benson <n@nben.net>
2019-09-24 14:53:40 -0400

    added a pmap() function for making persistent maps; also added a PersistentDefaultMap that acts as a PersistentArrayMap until the size is greater than 64 then switches to a PersistentHashMap

--- src/PersistentMap.jl
+++ src/PersistentMap.jl
@@ -215,3 +215,186 @@ Base.merge(d::PersistentHashMap, others...) =
     print(io, join(["$k => $v" for (k, v) in m], ", "))
     print(io, "]")
 end
+
+
+# Persistent Default Maps
+# =======================
+# The point of these "default" maps is this: it is generally fastest to use array maps for
+# small/short maps and best to use hash-maps for large maps. Usually, we don't want to
+# worry too much about tracking when things are large or small, though. Enter the default
+# maps. Default maps are just a struct that wraps and imitates a PersistentArrayMap with
+# one exception: when the size of an array map would grow to 64, a hash-map is instead
+# returned, so maps get promoted automatically.
+# The pmap function, below, returns default maps when appropriate instead of array maps.
+
+
+const _max_array_map_size = 64
+
+struct PersistentDefaultMap{K,V} <: PersistentMap{K,V}
+    impl::PersistentArrayMap{K,V}
+end
+
+PersistentDefaultMap{K, V}() where {K, V} =
+    PersistentDefaultMap{K, V}(PersistentArrayMap(Pair{K, V}[]))
+PersistentDefaultMap(arr::Array{Pair{K,V}}) where {K,V} =
+    PersistentDefaultMap{K,V}(PersistentArrayMap{K,V}(arr))
+PersistentDefaultMap(arr::Array{Tuple{K,V}}) where {K,V} =
+    PersistentDefaultMap{K,V}(PersistentArrayMap{K,V}([k=>v for (k,v) in arr]))
+PersistentDefaultMap(kvs::(Union{Tuple{K, V}, Pair{K, V}})...) where {K, V} =
+    PersistentDefaultMap{K,V}(PersistentArrayMap{K, V}(Pair{K, V}[Pair(k, v) for (k, v) in kvs]))
+Base.isequal(m1::PersistentDefaultMap, m2::PersistentDefaultMap) = isequal(m1.impl, m2.impl)
+==(m1::PersistentDefaultMap, m2::PersistentDefaultMap) = (m1.impl == m2.impl)
+
+Base.length(m::PersistentDefaultMap)  = length(m.impl)
+Base.isempty(m::PersistentDefaultMap) = isempty(m.impl)
+
+findkeyidx(m::PersistentDefaultMap, k) = findkeyidx(m.impl)
+
+Base.get(m::PersistentDefaultMap, k) = get(m.impl, k)
+Base.get(m::PersistentDefaultMap, k, default) = get(m.impl, k, default)
+Base.getindex(m::PersistentDefaultMap, k) = getindex(m.impl, k)
+
+Base.haskey(m::PersistentDefaultMap, k) = haskey(m.impl, k)
+
+function FunctionalCollections.assoc(m::PersistentDefaultMap{K, V}, k, v) where {K, V}
+    if length(m.impl) + 1 >= _max_array_map_size && !haskey(m.impl, k)
+        assoc(PersistentHashMap(collect(m)), k, v)
+    else
+        q = assoc(m.impl, k, v)
+        (q === m.impl) ? m : PersistentDefaultMap{K,V}(q)
+    end
+end
+
+function FunctionalCollections.dissoc(m::PersistentDefaultMap{K, V}, k) where {K, V}
+    q = dissoc(m.impl, k)
+    (q === m.impl) ? m : PersistentDefaultMap{K,V}(q)
+end
+
+function Base.iterate(m::PersistentDefaultMap, i = 1)
+    iterate(m.impl, i)
+end
+
+Base.map(f::( Union{DataType, Function}), m::PersistentDefaultMap) = PersistentDefaultMap(map(f, m.impl))
+
+Base.show(io::IO, q::MIME"text/plain", m::PersistentDefaultMap{K, V}) where {K, V} =
+    print(io, "PersistentDefault{$K, $V}$(m.impl.kvs)")
+
+# The pmap function:
+"""
+    pmap(map::PersistentMap)::PersistentMap
+Yields the given map.
+
+# Examples
+```jldoctest
+using FunctionalCollections
+m = FunctionalCollections.phmap([:a => 1, :b => 2, :c => 3])
+pmap(m) === m  # The identical map is returned.
+
+# output
+true
+```
+"""
+function pmap(map::PersistentMap{K,V})::PersistentMap{K,V} where {K,V}
+    map
+end
+"""
+    pmap(pairs::AbstractArray{Pair})::PersistentMap
+Yields a persistent map of the key-value pairs in the given array; if the size of the
+given arrary is greater than $_max_array_map_size, then a hash map is used; otherwise an array map is
+used.
+
+# Examples
+```jldoctest
+julia> pmap([:a => 1, :b => 2, :c => 3])
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+```
+"""
+function pmap(u::AbstractArray{Pair{K,V},1})::PersistentMap{K,V} where {K,V}
+    if length(u) < _max_array_map_size
+        PersistentDefaultMap(u)
+    else
+        PersistentHashMap(u)
+    end
+end
+"""
+    pmap(dict::AbstractDict{K,V})::PersistentMap{K,V}
+Yields a persistent map that is a duplicate of the given dictionary.
+
+# Examples
+```jldoctest
+julia> pmap(Dict(:a => 1, :b => 2, :c => 3))
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+```
+"""
+function pmap(dict::AbstractDict{K,V})::PersistentMap{K,V} where {K,V}
+    pmap(collect(dict))
+end
+"""
+    pmap(pairs::Pair{K,V}...)::PersistentMap{K,V}
+Yields a persistent map that is composed of the given key-value pairs.
+
+# Examples
+```jldoctest
+julia> pmap(:a => 1, :b => 2, :c => 3)
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+```
+"""
+function pmap(pairs::Pair{K,V}...)::PersistentMap{K,V} where {K,V}
+    pmap([pairs...])
+end
+"""
+    pmap(tuples::Tuple{K,V}...)::PersistentMap{K,V}
+Yields a persistent map that is composed of the given key-value tuples.
+
+# Examples
+```jldoctest
+julia> pmap((:a, 1), (:b, 2), (:c, 3))
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+```
+"""
+function pmap(tuples::Tuple{K,V}...)::PersistentMap{K,V} where {K,V}
+    pmap([tuples...])
+end
+"""
+    pmap()::PersistentMap{Any,Any}
+    pmap(k1=v1, k2=v2...)::PersistentMap{Any,Any}
+Yields an empty persistent map or an array map containing only the
+given key-value pairs.
+
+# Examples
+```jtdoctest
+julia> pmap()
+Persistent{Any, Any}Pair{Any,Any}[]
+julia> pmap(a=1, b=2)
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2]
+```
+"""
+function pmap(; kw...)::PersistentMap
+    if length(kw) < _max_array_map_size
+        PersistentDefaultMap([kw...])
+    else
+        PersistentHashMap([kw...])
+    end
+end
+"""
+    pmap(arr::Array)::PersistentMap
+Yields a persistent map from the generic array, which must contain any
+number of 2-tuples and pairs.
+
+# Examples
+```jtdoctest
+julia> pmap([(:a, 1), (:b, 2), (:c, 3)])
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+julia> pmap([:a => 1, :b => 2, :c => 3])
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+julia> pmap([:a => 1, (:b, 2), :c => 3])
+Persistent{Symbol, Int64}Pair{Symbol,Int64}[:a => 1, :b => 2, :c => 3]
+```
+"""
+function pmap(arr::Array)::PersistentMap
+    if length(arr) < _max_array_map_size
+        PersistentDefaultMap(arr)
+    else
+        PersistentHashMap(arr)
+    end
+end
